622e0baae5b9f1a8c7245ec9134a017a4c1a81df
SHANTO <rabbymd55555@gmail.com>
2025-03-18 23:55:20 +0600

    devborad all files

---
+++ index.js
@@ -0,0 +1,74 @@
+
+document.getElementById('completed-btn1').addEventListener('click',function(){
+    this.disabled = true;
+    const number = document.getElementById('task-numbers');
+    let currentNumber = parseInt(number.innerText);
+    number.innerText =currentNumber-1;
+
+    const plusNumber = document.getElementById('plus-number');
+    let plusCurrentNumber = parseInt(plusNumber.innerText);
+
+    // const history = document.getElementById('history');
+    // const historyList = document.createElement('p');
+    // historyList.innerHTML=`
+    // <p>first button</p>
+    // `
+    // history.appendChild(historyList)
+
+
+    plusNumber.innerText =plusCurrentNumber+1;
+})
+
+
+
+
+document.getElementById('completed-btn2').addEventListener('click',function(){
+    this.disabled = true;
+    const number = document.getElementById('task-numbers');
+    let currentNumber = parseInt(number.innerText);
+    number.innerText =currentNumber-1;
+
+    const plusNumber = document.getElementById('plus-number');
+    let plusCurrentNumber = parseInt(plusNumber.innerText);
+    plusNumber.innerText =plusCurrentNumber+1;
+})
+document.getElementById('completed-btn3').addEventListener('click',function(){
+    this.disabled = true;
+    const number = document.getElementById('task-numbers');
+    let currentNumber = parseInt(number.innerText);
+    number.innerText =currentNumber-1;
+
+    const plusNumber = document.getElementById('plus-number');
+    let plusCurrentNumber = parseInt(plusNumber.innerText);
+    plusNumber.innerText =plusCurrentNumber+1;
+})
+document.getElementById('completed-btn4').addEventListener('click',function(){
+    this.disabled = true;
+    const number = document.getElementById('task-numbers');
+    let currentNumber = parseInt(number.innerText);
+    number.innerText =currentNumber-1;
+
+    const plusNumber = document.getElementById('plus-number');
+    let plusCurrentNumber = parseInt(plusNumber.innerText);
+    plusNumber.innerText =plusCurrentNumber+1;
+})
+document.getElementById('completed-btn5').addEventListener('click',function(){
+    this.disabled = true;
+    const number = document.getElementById('task-numbers');
+    let currentNumber = parseInt(number.innerText);
+    number.innerText =currentNumber-1;
+
+    const plusNumber = document.getElementById('plus-number');
+    let plusCurrentNumber = parseInt(plusNumber.innerText);
+    plusNumber.innerText =plusCurrentNumber+1;
+})
+document.getElementById('completed-btn6').addEventListener('click',function(){
+    this.disabled = true;
+    const number = document.getElementById('task-numbers');
+    let currentNumber = parseInt(number.innerText);
+    number.innerText =currentNumber-1;
+
+    const plusNumber = document.getElementById('plus-number');
+    let plusCurrentNumber = parseInt(plusNumber.innerText);
+    plusNumber.innerText =plusCurrentNumber+1;
+})
